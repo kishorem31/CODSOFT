5d19e638b8daa38177f8862ed5a99e5c9a85902e
kishorem31 <kishorkamal1901@gmail.com>
2026-06-10 06:26:19 +0000

    ADDED

---
+++ NUMBER_GUESSING_GAME.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int secret = rand() % 50 + 1;
+    int guess;
+    cout << "Number Guessing Game" << endl;
+    cout << "Guess a number between 1 and 50" << endl;
+    do
+    {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        if (guess > secret)
+            cout << "Too High!" << endl;
+        else if (guess < secret)
+            cout << "Too Low!" << endl;
+        else
+            cout << "Congratulations! You guessed the correct number." << endl;
+    } while (guess != secret);
+    return 0;
+}
